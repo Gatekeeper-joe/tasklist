30ed6fc78663cd4c033ef96fcd9d34a17a370c7f
Gatekeeper-joe <investorjk5x@gmail.com>
2022-09-15 17:07:00 +0900

    コメント追加

--- src/main/java/com/example/tasklist/TaskListApplication.java
+++ src/main/java/com/example/tasklist/TaskListApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * タスク管理アプリケーションのメインクラス
+ */
 public class TaskListApplication {
 
+	/**
+	 * mainメソッド
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TaskListApplication.class, args);
 	}
